3242ad81a178232a2d349e25d8286cc0b104616d
Logan Bailey <logan@logansbailey.com>
2015-02-04 23:41:24 -0800

    Add GA ID to the site
    This isn’t probably the best way to do this, but it provides what we
    need.
    
    It hard codes site tracking to GA, but we know we’re using GA so I
    don’t see that being a problem. It provides flexibility, allowing
    user’s to define different UA’s for different machines.

--- app/config/services.php
+++ app/config/services.php
@@ -2,30 +2,32 @@
 
 return array(
 
-	/*
-	|--------------------------------------------------------------------------
-	| Third Party Services
-	|--------------------------------------------------------------------------
-	|
-	| This file is for storing the credentials for third party services such
-	| as Stripe, Mailgun, Mandrill, and others. This file provides a sane
-	| default location for this type of information, allowing packages
-	| to have a conventional place to find your various credentials.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, Mandrill, and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
 
-	'mailgun' => array(
-		'domain' => '',
-		'secret' => '',
-	),
+    'mailgun' => array(
+        'domain' => '',
+        'secret' => '',
+    ),
+    'mandrill' => array(
+        'secret' => '',
+    ),
+    'stripe' => array(
+        'model' => 'User',
+        'secret' => '',
+    ),
 
-	'mandrill' => array(
-		'secret' => '',
-	),
-
-	'stripe' => array(
-		'model'  => 'User',
-		'secret' => '',
-	),
+    'google-analytics' => [
+        'id' => isset($_ENV['GA_ID']) ? $_ENV['GA_ID'] : null
+    ]
 
 );
