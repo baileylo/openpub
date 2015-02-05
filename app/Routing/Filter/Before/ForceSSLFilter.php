d7d80d00f55434bdab7aef2fe1a9e8824f7d3c99
Logan Bailey <logan@logansbailey.com>
2015-02-05 08:58:17 -0800

    Fixes use of Request When Expected Redirector

--- app/Routing/Filter/Before/ForceSSLFilter.php
+++ app/Routing/Filter/Before/ForceSSLFilter.php
@@ -3,6 +3,7 @@
 namespace Baileylo\BlogApp\Routing\Filter\Before;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
 use Illuminate\Routing\Route;
 
 /**
@@ -10,10 +11,18 @@ use Illuminate\Routing\Route;
  */
 class ForceSSLFilter
 {
+    /** @var Redirector */
+    private $redirector;
+
+    public function __construct(Redirector $redirector)
+    {
+        $this->redirector = $redirector;
+    }
+
     public function filter(Route $route, Request $request)
     {
         if (!$request->isSecure()) {
-            return $request->secure($request->path());
+            return $this->redirector->secure($request->path());
         }
     }
 }
