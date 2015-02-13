2ed0644e892c2892eacef8763c71614fefd8752a
Logan Bailey <logan@logansbailey.com>
2015-02-13 15:38:16 -0800

    Updated middleware Fixes an incorrect URL redirect in the Authenticate middleware. Removes the now uneeded CSRF middleware requirement.

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,49 +2,52 @@
 
 use Closure;
 use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\Response;
 
-class Authenticate {
+class Authenticate
+{
+    /**
+     * The Guard implementation.
+     *
+     * @var Guard
+     */
+    protected $auth;
 
-	/**
-	 * The Guard implementation.
-	 *
-	 * @var Guard
-	 */
-	protected $auth;
+    /** @var Redirector */
+    private $redirector;
 
-	/**
-	 * Create a new filter instance.
-	 *
-	 * @param  Guard  $auth
-	 * @return void
-	 */
-	public function __construct(Guard $auth)
-	{
-		$this->auth = $auth;
-	}
+    /**
+     * Create a new filter instance.
+     *
+     * @param Guard      $auth
+     * @param Redirector $redirector
+     */
+    public function __construct(Guard $auth, Redirector $redirector)
+    {
+        $this->auth = $auth;
+        $this->redirector = $redirector;
+    }
 
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next)
-	{
-		if ($this->auth->guest())
-		{
-			if ($request->ajax())
-			{
-				return response('Unauthorized.', 401);
-			}
-			else
-			{
-				return redirect()->guest('auth/login');
-			}
-		}
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!$this->auth->guest()) {
+            return $next($request);
+        }
 
-		return $next($request);
-	}
+        if ($request->ajax()) {
+            return new Response('Unauthorized.', 401);
+        }
+
+        return $this->redirector->guest('login');
+    }
 
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -25,7 +25,7 @@ Route::get('feed', [
     'as' => 'feed.atom'
 ]);
 
-Route::group(['before' => 'auth'], function () {
+Route::group(['middleware' => 'auth'], function () {
 
     Route::get('/import', ['uses' => 'Admin\ImportView@view', 'as' => 'import']);
     Route::post('/import', ['uses' => 'Admin\ImportHandler@import']);
@@ -50,29 +50,27 @@ Route::group(['before' => 'auth'], function () {
     Route::get('{slug}/edit', ['uses' => 'Resource\Edit@view', 'as' => 'page.edit']);
     Route::get('{slug}/edit', ['uses' => 'Resource\Edit@view', 'as' => 'post.edit']);
 
-    Route::group(['before' => 'csrf'], function () {
-        // Delete Page Or Post
-        Route::delete('{slug}', ['uses' => 'Resource\Delete@delete', 'as' => 'page.delete']);
-        Route::delete('{slug}', ['uses' => 'Resource\Delete@delete', 'as' => 'post.delete']);
+    // Delete Page Or Post
+    Route::delete('{slug}', ['uses' => 'Resource\Delete@delete', 'as' => 'page.delete']);
+    Route::delete('{slug}', ['uses' => 'Resource\Delete@delete', 'as' => 'post.delete']);
 
-        // Handle Edits to Page
-        Route::put('{slug}/edit', ['uses' => 'Resource\EditHandler@handle']);
-        Route::post('{slug}/edit', ['uses' => 'Resource\EditHandler@handle']);
+    // Handle Edits to Page
+    Route::put('{slug}/edit', ['uses' => 'Resource\EditHandler@handle']);
+    Route::post('{slug}/edit', ['uses' => 'Resource\EditHandler@handle']);
 
-        // Create Page/Slug
-        Route::post('/write', ['uses' => 'Post\Create\CreatePostHandler@handle']);
-        Route::post('/write/page', ['uses' => 'Page\Create\Handler@handleForm', 'as' => 'admin.page.create']);
+    // Create Page/Slug
+    Route::post('/write', ['uses' => 'Post\Create\CreatePostHandler@handle']);
+    Route::post('/write/page', ['uses' => 'Page\Create\Handler@handleForm', 'as' => 'admin.page.create']);
 
-        // Publish / Unpublish Page
-        Route::patch('{slug}/publish', ['uses' => 'Resource\Publish@publish', 'as' => 'post.publish']);
-        Route::patch('{slug}/publish', ['uses' => 'Resource\Publish@publish', 'as' => 'page.publish']);
+    // Publish / Unpublish Page
+    Route::put('{slug}/publish', ['uses' => 'Resource\Publish@publish', 'as' => 'post.publish']);
+    Route::put('{slug}/publish', ['uses' => 'Resource\Publish@publish', 'as' => 'page.publish']);
 
-        Route::patch('{slug}/unpublish', ['uses' => 'Resource\Unpublish@unpublish', 'as' => 'post.unpublish']);
-        Route::patch('{slug}/unpublish', ['uses' => 'Resource\Unpublish@unpublish', 'as' => 'page.unpublish']);
+    Route::patch('{slug}/unpublish', ['uses' => 'Resource\Unpublish@unpublish', 'as' => 'post.unpublish']);
+    Route::patch('{slug}/unpublish', ['uses' => 'Resource\Unpublish@unpublish', 'as' => 'page.unpublish']);
 
-        Route::put('/settings', ['uses' => 'User\Settings\SettingsHandler@handleForm', 'before' => 'forceSSL']);
-        Route::put('/update-password', ['uses' => 'User\Settings\PasswordHandler@handleForm', 'as' => 'passwordHandler']);
-    });
+    Route::put('/settings', ['uses' => 'User\Settings\SettingsHandler@handleForm', 'before' => 'forceSSL']);
+    Route::put('/update-password', ['uses' => 'User\Settings\PasswordHandler@handleForm', 'as' => 'passwordHandler']);
 });
 
 Route::get('{date}/{slug}', ['before' => 'postUrlRedirect']);
