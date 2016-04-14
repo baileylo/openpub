9ef4eccb389768cb65977f070ec25f9452619e7e
Logan Bailey <logan@logansbailey.com>
2016-04-14 07:00:21 -0700

    Set view port

--- resources/views/layouts/basic.blade.php
+++ resources/views/layouts/basic.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title')</title>
+    <meta name=viewport content="width=device-width, initial-scale=1">
 
     <link rel="alternate" type="application/atom+xml" title="Logan Bailey &mdash; Adventures In Web Development" href="{{ route('feed') }}" />
 
