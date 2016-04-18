db156f9effcbb3996fad9778c6251dd17d38f5cd
Logan Bailey <logan@logansbailey.com>
2016-04-18 14:38:15 -0700

    Safari Admin Fixes
    Small minor html fixes for safari.

--- resources/views/post/overview.blade.php
+++ resources/views/post/overview.blade.php
@@ -19,13 +19,11 @@
                         <a href="{{ route('resource', $post->slug) }}">{{ $post->title }}</a>
                         <div class="visible-for-small-down">
                             <a href="{{ route('post.edit', $post->slug) }}" title="Edit"><i class="fa fa-pencil"></i></a>
-                            {{--{{ Form::csrfLink('<i class="fa fa-remove"></i>', ['route' => ['post.unpublish', $post->getSlug()], 'method' => 'patch', 'title' => 'Un Publish']) }}--}}
-                            {{--{{ Form::csrfLink('<i class="fa fa-trash"></i>', ['route' => ['post.delete', $post->getSlug()], 'method' => 'delete', 'title' => 'Delete']) }}--}}
                         </div>
                     </td>
                     <td class="hidden-for-small-down">{{ $post->published_at ? $post->published_at->format('F jS, Y') : 'Unknown' }}</td>
                     <td class="hidden-for-small-down">
-                        <a href="{{ route('post.edit', $post->slug) }}" title="Edit"><i class="fa fa-pencil"></i></a>
+                        <a href="{{ route('post.edit', $post->slug) }}" title="Edit" class="label round"><i class="fa fa-pencil"></i></a>
                         @include('partials.delete-link', ['url' => route('post.destroy', $post->slug)])
                     </td>
                 </tr>
@@ -34,7 +32,9 @@
             @if($pending->count())
                 <tfoot>
                     <tr>
-                        {{--<td colspan="4" class="text-right"><a href="{{ route('admin.unpublished') }}">See All</a></td>--}}
+                        <td colspan="3" class="text-right">
+                            {{--<a href="{{ route('admin.unpublished') }}">See All</a>--}}
+                        </td>
                     </tr>
                 </tfoot>
             @endif
@@ -67,7 +67,7 @@
         @endforeach
         <tfoot>
         <tr>
-            <td colspan="4" class="hidden-for-small-down">
+            <td colspan="3" class="hidden-for-small-down">
                 {!! $published->render() !!}
             </td>
         </tr>
