d36e7bc18547388d0d44f6bc5cfc014eed52dffc
Logan Bailey <logan@logansbailey.com>
2016-04-20 09:09:05 -0700

    Change Code Highlight To vs.css from github.css

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix
         .sass('app.scss')
-        .styles(['highlight/github.css'], 'public/css/highlight.css')
+        .styles(['highlight/vs.css'], 'public/css/highlight.css')
         .scripts('highlight.pack.js', 'public/js/highlight.js')
         .scripts(['bootstrap.js', 'bootstrap-sprockets.js'], 'public/js/admin.js')
         .version(['js/admin.js', 'css/app.css', 'js/highlight.js', 'css/highlight.css']);
