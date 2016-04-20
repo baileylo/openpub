2b8eed2e2a0fed7cb082940ffb6907815545fc37
Logan Bailey <logan@logansbailey.com>
2016-04-19 23:53:56 -0700

    Mobile Specific Header

--- resources/views/layouts/full.blade.php
+++ resources/views/layouts/full.blade.php
@@ -8,24 +8,34 @@
 
 @section('header')
     <div class="row">
-        <div class="col-xs-12 col-md-12 text-center">
-            <img src="/img/profile@2x.jpg" alt="Logan Bailey" class="img-thumbnail" width="150">
+        <div class="default-view col-md-12 text-center visible-md-block visible-sm-block visible-lg-block">
+            <div class="visible-md-inline-block visible-sm-inline-block visible-lg-block image-wrapper">
+                <img src="/img/profile@2x.jpg" alt="Logan Bailey" class="img-thumbnail" width="150">
+            </div>
+            <div class="visible-md-inline-block visible-sm-inline-block visible-lg-block">
+                <h1>Adventures In Web Development</h1>
+                <p>
+                    <a href="{{ route('home') }}" title="blog">Blog</a>,
+                    <a href="{{ route('resource', 'about-me') }}">About</a>,
+                    <a href="https://github.com/baileylo" target="_blank">GitHub</a>, and
+                    <a href="https://www.linkedin.com/in/loganbailey" target="_blank">LinkedIn</a>
+                </p>
+            </div>
+        </div>
+        <div class="col-xs-12 visible-xs-block mobile-view">
+            <a href="{{ route('home') }}">
+                <img src="/img/profile@2x.jpg" alt="Logan Bailey" class="img-circle" width="35">
+            </a>
             <h1>
-                <small>Adventures In Web Development</small>
+                <a href="{{ route('home') }}">Adventures In Web Development</a>
             </h1>
-            <p>
-                <a href="{{ route('home') }}" title="blog">Blog</a>,
-                <a href="{{ route('resource', 'about-me') }}">About</a>,
-                <a href="https://github.com/baileylo" target="_blank">GitHub</a>, and
-                <a href="https://www.linkedin.com/in/loganbailey" target="_blank">LinkedIn</a>
-            </p>
         </div>
     </div>
 @stop
 
 @section('footer')
     <div class="row">
-        <div class="col-xs-12 col-md-2 col-md-push-5">
+        <div class="col-xs-7 col-xs-push-5 col-md-2 col-md-push-5">
             <div class="footer-content">
                 <ul class="list-unstyled">
                     <li class="heading">Follow Me</li>
